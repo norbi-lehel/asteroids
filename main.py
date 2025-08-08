fa663fe00faec5e27ed49a04cf0e88b65cc1a0c4
sipos98 <lehel1991@gmail.com>
2025-08-08 12:52:59 +0300

    2nd commit

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -16,6 +20,8 @@ def main():
         screen.fill("black")
         pygame.display.flip()
 
+
+        dt = clock.tick(60) / 1000
 
     print ("Starting Asteroids!")
     print (f"Screen width: {SCREEN_WIDTH}")
